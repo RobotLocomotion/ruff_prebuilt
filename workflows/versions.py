c99c00bdd4f4b0550fcd638c108a9b40636a58a3
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-09-11 10:57:56 -0700

    Add upgrade automation

---
+++ workflows/versions.py
@@ -0,0 +1,120 @@
+"""
+Automates upkeep of version.json.
+"""
+
+import argparse
+import base64
+import json
+from pathlib import Path
+import urllib.request
+
+from python import runfiles
+
+# Maps from rust triples to @platforms//cpu and @platforms//os constants.
+_TRIPLE_TO_CPU_OS = {
+    "aarch64-apple-darwin": ("aarch64", "macos"),
+    "aarch64-unknown-linux-gnu": ("aarch64", "linux"),
+    "armv7-unknown-linux-gnueabihf": ("armv7", "linux"),
+    "i686-unknown-linux-gnu": ("x86_32", "linux"),
+    "powerpc64le-unknown-linux-gnu": ("ppc64le", "linux"),
+    "riscv64gc-unknown-linux-gnu": ("riscv64", "linux"),
+    "s390x-unknown-linux-gnu": ("s390x", "linux"),
+    "x86_64-apple-darwin": ("x86_64", "macos"),
+    "x86_64-unknown-linux-gnu": ("x86_64", "linux"),
+}
+
+
+def _versions_path() -> Path:
+    manifest = runfiles.Create()
+    path = Path(manifest.Rlocation("_main/versions.json"))
+    assert path.exists(), path
+    return path.resolve()
+
+
+def _download(url: str, *, encoding: str | None) -> bytes:
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+    if encoding is None:
+        return data
+    return data.decode(encoding=encoding)
+
+
+def _sha256_hex_to_integrity(as_hex: str) -> str:
+    as_base64 = base64.b64encode(bytes.fromhex(as_hex)).decode("utf-8")
+    return f"sha256-{as_base64}"
+
+
+def _rewrite_versions_json(edits_callback) -> None:
+    path = _versions_path()
+    root = json.loads(path.read_text(encoding="utf-8"))
+    edits_callback(root)
+    path.write_text(json.dumps(root, indent=1), encoding="utf-8")
+
+
+def _add(args: argparse.Namespace) -> None:
+    version = args.version
+    base_url = f"https://github.com/astral-sh/ruff/releases/download/{version}"
+    manifest_url = f"{base_url}/dist-manifest.json"
+    manifest = json.loads(_download(manifest_url, encoding="utf-8"))
+
+    download_specs = {}
+    for artifact in manifest["artifacts"].values():
+        name = artifact["name"]
+        if not name.endswith(".tar.gz"):
+            continue
+        if "target_triples" not in artifact:
+            continue
+        (triple,) = artifact["target_triples"]
+        cpu, os = _TRIPLE_TO_CPU_OS.get(triple, (None, None))
+        if cpu is None:
+            continue
+        sha256_url = f"{base_url}/{name}.sha256"
+        sha256_hex = _download(sha256_url, encoding="utf-8").split()[0]
+        integrity = _sha256_hex_to_integrity(sha256_hex)
+        strip_prefix = Path(Path(name).stem).stem
+        url = f"{base_url}/{name}"
+        download_spec = dict(
+            cpu=cpu,
+            integrity=integrity,
+            os=os,
+            strip_prefix=strip_prefix,
+            urls=[url],
+        )
+        download_specs[name] = download_spec
+
+    def _edits(root):
+        root["available"][version] = dict(downloads=download_specs)
+
+    _rewrite_versions_json(_edits)
+
+
+def _sync(args: argparse.Namespace) -> None:
+    raise NotImplementedError()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    subparsers = parser.add_subparsers()
+    parser_add = subparsers.add_parser(
+        "add",
+        help="Adds a new version to the database (or overwrites the existing one).",
+    )
+    parser_add.add_argument(
+        "version",
+        help="The version number to be added (or updated).",
+    )
+    parser_add.set_defaults(handler=_add)
+    parser_sync = subparsers.add_parser(
+        "sync",
+        help="Updates all versions currently in the database.",
+    )
+    parser_sync.set_defaults(handler=_sync)
+    args = parser.parse_args()
+    args.handler(args)
+
+
+assert __name__ == "__main__"
+main()
